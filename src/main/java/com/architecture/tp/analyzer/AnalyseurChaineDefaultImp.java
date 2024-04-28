1b607bc5db9df3349bf441d10aaaf6f2889e3078
achahbar <shadys.50c@gmail.com>
2024-04-28 21:47:33 +0200

    etape2  la chaine renvoiyé commence toujours par bonjour et se termine par au revoir  et si c est un palindrome renvoie le mirroir suivit de bien dis

--- src/main/java/com/architecture/tp/analyzer/AnalyseurChaineDefaultImp.java
+++ src/main/java/com/architecture/tp/analyzer/AnalyseurChaineDefaultImp.java
@@ -9,7 +9,7 @@ public class AnalyseurChaineDefaultImp implements AnalyzeurChaine{
 	@Override
 	public String AnalyserChaine(String chaine) {
 		String mirroir = new StringBuilder(chaine).reverse().toString();
-		return mirroir.equals(chaine)?"bien dit "+mirroir:mirroir;
+		return mirroir.equals(chaine)?"Bonjour\n"+mirroir+" bien dit\nau revoir ":"Bonjour\n"+mirroir+"\nau revoir";
 	}
 
 }
