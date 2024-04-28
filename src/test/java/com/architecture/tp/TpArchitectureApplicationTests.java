7b0a0a259a279245a2a4c8a4b11dbc3a2a62a5b8
achahbar <shadys.50c@gmail.com>
2024-04-28 11:35:52 +0200

    first commit empty project

---
+++ src/test/java/com/architecture/tp/TpArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.architecture.tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
