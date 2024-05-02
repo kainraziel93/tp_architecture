fdf863e1c06d1d4732d036935bb0ffaf8822f211
achahbar <shadys.50c@gmail.com>
2024-05-02 21:15:49 +0200

    ajout de deux implementation de l'interface language->francais et anglais

---
+++ src/main/java/com/architecture/tp/language/Francais.java
@@ -0,0 +1,23 @@
+package com.architecture.tp.language;
+
+public class Francais implements Language{
+
+	@Override
+	public String saluer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String feliciter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String acquitter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
